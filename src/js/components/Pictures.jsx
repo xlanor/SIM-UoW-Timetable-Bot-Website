267e9cf3a217c0d3877f85d5b90a2cfc0b8741bf
xlanor <tanjingkai@me.com>
2018-11-21 14:28:08 +0800

    Began prep work for adding an additional component (breaking up the pictures into sub-components

--- src/js/components/Pictures.jsx
+++ src/js/components/Pictures.jsx
@@ -4,11 +4,54 @@ export default class Pictures extends React.Component {
     
 
     render() {
-      const displayFunctionOne = `
-                                    A scrollable timetable to display 
-                                    all the timetables that are scraped from
-                                    SIMConnect.
-                                `
+      const videoDetails =[
+          {
+              "title":"Scrollable Timetable",
+              "mp4":"../src/videos/scrollable_timetable.mp4",
+              "webm":"../src/videos/scrollable_timetable.webm",
+              "bullet_points":[
+                "Pulls all timetables from SIMConnect",
+                "Inline scrolling with Telegram"
+              ],
+              "sub_text":[
+                  "* All timetables only includes classes and IS timetables",
+                  "** Subject to Telegram rate limits",
+                  "*** Does NOT automatically update"
+              ]
+          },
+          {
+            "title":"Daily Alerts",
+            "mp4":"../src/videos/notification_alert.mp4",
+            "webm":"../src/videos/notification_alert.webm",
+            "bullet_points":[
+              "Notifications at 8am and 10pm every day.",
+              "Provide details of upcoming classes",
+              "Never miss another class again!"
+            ],
+            "sub_text":[
+                "* Does not guarantee a safeguard against oversleeping"
+            ]
+          },
+          {
+            "title":"Chatbot Signup",
+            "mp4":"../src/videos/signup_tt_v2.mp4",
+            "webm":"../src/videos/signup_tt_v2.webm",
+            "bullet_points":[
+              "Talk to get started",
+              "Comprehensive documentation on Github"
+            ],
+            "sub_text":[]
+          },
+          {
+            "title":"Reduced Complexity",
+            "mp4":"../src/videos/sim_navigation.mp4",
+            "webm":"../src/videos/sim_navigation.webm",
+            "bullet_points":[
+              "No more suffering with an interface clearly not designed for mobile"
+            ],
+            "sub_text":[]
+          }
+      ]
       return (
         <div id="pictures-div">
             <div className="wrapper-div">
@@ -25,16 +68,13 @@ export default class Pictures extends React.Component {
                         ⇒ Pulls all timetables from SIMConnect 
                         <br/>
                         ⇒ Inline scrolling with Telegram
-                        <br/><br/>
-                        <br/><br/>
-                        <br/><br/>
-                        <div className = "overlay-subtext">
-                            <i>* All timetables only includes classes and IS timetables</i>
-                            <br/>
-                            <i>** Subject to Telegram rate limits</i>
-                            <br/>
-                            <i>*** Does NOT automatically update.</i>
-                        </div>
+                    </div>
+                    <div className = "overlay-subtext">
+                        <i>* All timetables only includes classes and IS timetables</i>
+                        <br/>
+                        <i>** Subject to Telegram rate limits</i>
+                        <br/>
+                        <i>*** Does NOT automatically update.</i>
                     </div>
                 </div>
             </div>
@@ -55,11 +95,10 @@ export default class Pictures extends React.Component {
                         ⇒ Provide details of upcoming classes
                         <br/>
                         ⇒ Never miss another class again! *
-                        <br/><br/><br/><br/>
-                        <div className = "overlay-subtext">
+                    </div>
+                    <div className = "overlay-subtext">
                             <i>* Does not guarantee a safeguard against oversleeping</i>
                             <br/>
-                        </div>
                     </div>
                 </div>
             </div>
